8b24a3b1cfb49f982f924bf16143b1cebf2b1cc5
YuJeeun <dbwpdms1122@naver.com>
2025-02-01 15:14:49 +0900

    [Gold IV] Title: 알파벳, Time: 6024 ms, Memory: 156008 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,3 +1,34 @@
+# import sys
+
+# inp = sys.stdin.readline
+
+# r, c = map(int, inp().split())
+
+# board = [inp().strip() for _ in range(r)]
+
+# dx = [1, 0, -1, 0]
+# dy = [0, 1, 0, -1]
+
+# visit = set()
+
+# max_count = 0
+
+# def dfs(x, y, count):
+#     global max_count
+#     max_count = max(max_count, count)
+#     for i in range(4):
+#         nx = dx[i] + x
+#         ny = dy[i] + y
+#         if nx < 0 or ny < 0 or nx >= r or ny >= c or board[nx][ny] in visit: continue
+#         visit.add(board[nx][ny])
+#         dfs(nx, ny, count+1)
+#         visit.remove(board[nx][ny])
+
+# visit.add(board[0][0])
+# dfs(0, 0, 1)
+# print(max_count)
+
+
 import sys
 
 inp = sys.stdin.readline
@@ -6,30 +37,30 @@ r, c = map(int, inp().split())
 
 board = [inp().strip() for _ in range(r)]
 
-dx = [-1, 0, 1, 0]
+dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-result = 0
+visit = [[False] * c for _ in range(r)]
 
-def dfs(x, y, bitmask, count): # visit 대신 vitmask 사용 A는 0, B는 1, ...
-    global result
-    result = max(result, count)
+alphabet = [False] * 26
 
+max_count = 0
+
+def dfs(x, y, count):
+    global max_count
+    max_count = max(max_count, count)
+    
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx = dx[i] + x
+        ny = dy[i] + y
+        if nx < 0 or ny < 0 or nx >= r or ny >= c or visit[nx][ny] or alphabet[ord(board[nx][ny]) - 65]: continue
+        visit[nx][ny] = True
+        alphabet[ord(board[nx][ny]) - 65] = True
+        dfs(nx, ny, count+1)
+        visit[nx][ny] = False
+        alphabet[ord(board[nx][ny]) - 65] = False
 
-        if nx < 0 or ny < 0 or nx >= r or ny >= c: continue
-        next_char = board[nx][ny]
-        next_bit = 1 << (ord(next_char) - ord('A'))
-
-        if bitmask & next_bit:
-            continue
-
-        dfs(nx, ny, bitmask | next_bit, count+1)
-
-start_char = board[0][0]
-start_bitmask = 1 << (ord(start_char) - ord('A'))
-dfs(0,0,start_bitmask,1)
-
-print(result)
+visit[0][0] = True
+alphabet[ord(board[0][0]) - 65] = True
+dfs(0, 0, 1)
+print(max_count)
