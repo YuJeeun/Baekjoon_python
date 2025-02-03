26e64035c7fb4282026ed6f9249acf42f420d162
YuJeeun <dbwpdms1122@naver.com>
2025-02-03 12:10:54 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,12 +1,10 @@
-# 일반 조합 1,2랑 2,1은 같은거임 순서가 없으니까
-
 import sys
 
 inp = sys.stdin.readline
 
 n, m = map(int, inp().split())
 
-arr = list()
+arr = []
 for i in range(n):
     arr.append(i+1)
 
@@ -18,7 +16,7 @@ def dfs(idx):
             print(i, end=' ')
         print()
         return
-
+    
     for i in range(idx, n):
         result.append(arr[i])
         dfs(i+1)
