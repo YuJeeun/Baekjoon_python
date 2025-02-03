482b70b9141963cb481463ac7caf0edf7072d6e7
YuJeeun <dbwpdms1122@naver.com>
2025-02-03 12:17:53 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,24 +1,5 @@
-'''
-자연수 n, m
-- 1부터 n까지 자연수 중 m개를 고른 수열
-- 같은 수 여러 번 -> 중복 O
-- 비내림차순
-
-중복되는 수열을 여러 번 출력할 수 없음
-1 1
-1 2
-1 3
-1 4
-2 2
-2 3
-2 4
-3 3
-3 4
-4 4
--> 중복 조합
-'''
-
 import sys
+
 inp = sys.stdin.readline
 
 n, m = map(int, inp().split())
@@ -42,4 +23,3 @@ def dfs(idx):
         result.pop()
 
 dfs(0)
-
