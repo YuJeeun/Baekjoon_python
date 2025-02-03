9ad0a0b38c9b952e587edb00fdb00d30fefd4198
YuJeeun <dbwpdms1122@naver.com>
2025-02-03 13:10:28 +0900

    [Silver II] Title: N과 M (12), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,30 @@
+# 중복값이 있는 중복 조합
+
+import sys
+
+inp = sys.stdin.readline
+
+n, m = map(int, inp().split())
+
+arr = list(map(int, inp().split()))
+arr.sort()
+
+result = []
+
+def dfs(idx):
+    if len(result) == m:
+        for i in result:
+            print(i, end=' ')
+        print()
+        return
+
+    tmp = 0
+
+    for i in range(idx, n):
+        if tmp == arr[i]: continue
+        result.append(arr[i])
+        tmp = arr[i]
+        dfs(i)
+        result.pop()
+
+dfs(0)
