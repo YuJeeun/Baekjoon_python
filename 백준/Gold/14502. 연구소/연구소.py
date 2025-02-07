af14f97aea94d18c3bfe91df3809f453e40a05fe
YuJeeun <dbwpdms1122@naver.com>
2025-02-07 15:03:51 +0900

    [Gold IV] Title: 연구소, Time: 1684 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -7,23 +7,25 @@ n, m = map(int, inp().split())
 
 lab = [list(map(int, inp().split())) for _ in range(n)]
 
+visit = [[0]*m for _ in range(n)]
+
 empty_area = []
 for i in range(n):
     for j in range(m):
         if lab[i][j] == 0:
             empty_area.append([i, j])
-            
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+
+wall = []
 
 queue = deque()
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 cur = [0]*2
-visit = [[0]*m for _ in range(n)]
 
 res = 0
 
-wall = []
 def build_wall(idx):
     if len(wall) == 3:
         bfs()
@@ -42,7 +44,7 @@ def bfs():
     visit = [[0]*m for _ in range(n)]
     for i in range(n):
         for j in range(m):
-            if lab[i][j] == 2 and visit[i][j] == 0:
+            if visit[i][j] == 0 and lab[i][j] == 2:
                 queue.append([i, j])
                 visit[i][j] = 1
                 while queue:
@@ -50,14 +52,16 @@ def bfs():
                     for k in range(4):
                         nx = dx[k] + cur[0]
                         ny = dy[k] + cur[1]
-                        if nx < 0 or ny < 0 or nx >=n or ny >= m or lab[nx][ny] == 1 or visit[nx][ny] == 1: continue
+                        if nx < 0 or ny < 0 or nx >= n or ny >= m or visit[nx][ny] == 1 or lab[nx][ny] == 1: continue
                         visit[nx][ny] = 1
                         queue.append([nx, ny])
+    
     for i in range(n):
         for j in range(m):
             if visit[i][j] == 0 and lab[i][j] == 0:
                 tmp_res += 1
-    res = max(tmp_res, res)
+    res = max(res, tmp_res)
 
 build_wall(0)
 print(res)
+
