1147c580305caaae0cdc4004d8615daa9f1c49cc
YuJeeun <dbwpdms1122@naver.com>
2025-02-11 12:31:31 +0900

    [Gold IV] Title: 최단경로, Time: 544 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -5,22 +5,24 @@ inp = sys.stdin.readline
 
 v_num, e = map(int, inp().split())
 start = int(inp())
-INF = int(1e9)
+
 graph = [[] for _ in range(v_num+1)]
 
 for i in range(e):
     u, v, w = map(int, inp().split())
-    graph[u].append((v,w)) # u -> v 가중치 w
+    graph[u].append((v, w)) # u->v 가중치 w
 
+INF = int(1e9)
 distance = [INF] * (v_num+1)
+
 def dijkstra(start):
     queue = []
-    heapq.heappush(queue, (0, start))
-    distance[start] = 0
+    distance[start] = 0 
+    heapq.heappush(queue, (distance[start], start)) # 거리, 시작정점
     while queue:
-        dist, now = heapq.heappop(queue) # 거리, 현재 정점
-        if distance[now] < dist: continue # 이미 진행된 단계면 continue
-        
+        dist, now = heapq.heappop(queue)
+        if distance[now] < dist: continue
+
         for i in graph[now]:
             cost = dist + i[1]
             if cost < distance[i[0]]:
@@ -29,7 +31,8 @@ def dijkstra(start):
 
 dijkstra(start)
 for i in range(1, len(distance)):
-    if distance[i] == INF:
+    if distance[i]==INF:
         print('INF')
-    else:
-        print(distance[i])
+    else: print(distance[i])
+            
+
