54ca0d504442efd0a870b5289d732c8aec62517d
YuJeeun <dbwpdms1122@naver.com>
2025-02-17 17:30:31 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -4,62 +4,43 @@ from collections import deque
 inp = sys.stdin.readline
 
 n = int(inp())
-
-grid = [[0]*n for _ in range(n)]
-visit = [[0]*n for _ in range(n)]
+arr1 = [[0]*n for _ in range(n)]
+arr2 = [inp().strip() for _ in range(n)]
 
 for i in range(n):
-    rgb = inp()
     for j in range(n):
-        grid[i][j] = rgb[j]
+        arr1[i][j] = arr2[i][j]
 
 queue = deque()
 cur = [0]*2
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-result1 = 0
-result2 = 0
-for i in range (n):
-    for j in range(n):
-        if visit[i][j] == 0:
-            queue.append([i, j])
-            while queue:
-                cur = queue.popleft()
-                visit[cur[0]][cur[1]] = 1
-                for k in range (4):
-                    nx = cur[0] + dx[k]
-                    ny = cur[1] + dy[k]
-                    if nx < 0 or ny < 0 or nx >= n or ny >= n or grid[nx][ny] != grid[cur[0]][cur[1]]: continue
-                    if visit[nx][ny] == 0:
+def bfs():
+    cnt = 0
+    visit = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if visit[i][j] == 0:
+                queue.append((i, j))
+                visit[i][j] = 1
+                while queue:
+                    cur = queue.popleft()
+                    for k in range(4):
+                        nx = dx[k] + cur[0]
+                        ny = dy[k] + cur[1]
+                        if nx < 0 or ny < 0 or nx >= n or ny >= n or arr1[nx][ny] != arr1[cur[0]][cur[1]] or visit[nx][ny] == 1: continue
+                        queue.append((nx, ny))
                         visit[nx][ny] = 1
-                        queue.append([nx, ny])
-            result1 += 1
-print(result1, end=' ')
+                cnt += 1
+    return cnt
 
-visit = [[0]*n for _ in range(n)]
+print(bfs(), end=' ')
 
-for i in range (n):
+for i in range(n):
     for j in range(n):
-        if grid[i][j] == 'G':
-            grid[i][j] = 'R'
+        if arr1[i][j] == 'R':
+            arr1[i][j] = 'G'
 
-for i in range (n):
-    for j in range(n):
-        if visit[i][j] == 0:
-            queue.append([i, j])
-            while queue:
-                cur = queue.popleft()
-                visit[cur[0]][cur[1]] = 1
-                for k in range (4):
-                    nx = cur[0] + dx[k]
-                    ny = cur[1] + dy[k]
-                    if nx < 0 or ny < 0 or nx >= n or ny >= n or grid[nx][ny] != grid[cur[0]][cur[1]]: continue
-                    if visit[nx][ny] == 0:
-                        visit[nx][ny] = 1
-                        queue.append([nx, ny])
-            result2 += 1
-print(result2)
-
-
-
+print(bfs())
