017f22f9ad16e41ad009e79ebd0ef86bf685a7c5
YuJeeun <dbwpdms1122@naver.com>
2025-02-17 15:44:26 +0900

    [Gold V] Title: 토마토, Time: 2368 ms, Memory: 100544 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,44 +2,52 @@ import sys
 from collections import deque
 
 inp = sys.stdin.readline
-m, n = map(int, inp().split()) #y, x
-matrix = [list(map(int, inp().strip().split())) for _ in range(n)]
+
+'''
+m: 가로, n: 세로
+1: 익은 토마토, 0: 안익은 토마토, -1: 빈 칸
+* 토마토는 하나 이상 존재
+토마토가 모두 익을 때까지의 최소 날짜
+이미 모두 익어있다면 0, 모두 익을 수 없는 상황이라면 -1
+'''
+
+m, n = map(int, inp().split())
+
+box = [list(map(int, inp().split())) for _ in range(n)]
+
+queue = deque()
+cur = [0]*3
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-queue = deque()
-cur = [0]*3
-res = 0
 zero = 0
-cnt = 0
 
 for i in range(n):
-    zero += matrix[i].count(0)
+    for j in range(m):
+        if box[i][j] == 0:
+            zero += 1
+        if box[i][j] == 1:
+            queue.append((i, j, 0))
+            box[i][j] = -2
 
 if zero == 0:
     print(0)
     exit()
 
-for i in range(n):
-    for j in range(m):
-        if matrix[i][j] == 1:
-            queue.append([i, j, 0])
-            matrix[i][j] = 2
 while queue:
     cur = queue.popleft()
-    for k in range (4):
-        nx = cur[0] + dx[k]
-        ny = cur[1] + dy[k]
-        if nx < 0 or ny < 0 or nx >= n or ny >= m or matrix[nx][ny]==-1: continue
-        if matrix[nx][ny] == 0:
-            matrix[nx][ny] = 2
-            queue.append([nx, ny, cur[2]+1])
-            cnt += 1
-res+=cur[2]
-if zero == cnt:
-    print(res)
-    exit()
-else:
-    print(-1)
-    exit()
+    for k in range(4):
+        nx = dx[k] + cur[0]
+        ny = dy[k] + cur[1]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m or box[nx][ny] != 0: continue
+        box[nx][ny] = -2
+        queue.append((nx, ny, cur[2]+1))
+
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 0:
+            print(-1)
+            exit()
+            
+print(cur[2])
