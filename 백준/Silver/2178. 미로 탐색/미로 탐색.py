8ab7d80866522fb4456f927f6c852d22a1a54d89
YuJeeun <dbwpdms1122@naver.com>
2025-02-17 15:11:05 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,33 @@
 import sys
 from collections import deque
+
 inp = sys.stdin.readline
 
 n, m = map(int, inp().split())
 
-matrix = [[0] * m for _ in range(n)]
-
+arr = [inp().strip() for _ in range(n)]
+maze = [[0]*m for _ in range(n)]
 for i in range(n):
-    str = inp().strip()
-    for j in range(len(str)):
-        matrix[i][j] = int(str[j])
+    for j in range(m):
+        maze[i][j] = int(arr[i][j])
 
 queue = deque()
-queue.append([0, 0, 1])
+queue.append((0, 0, 1))
+maze[0][0] = -1
+cur = [0]*3
 
-# 북 동 남 서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-arr = [0] * 3
-matrix[0][0] = 2
 
-while len(queue) !=0: # 큐가 빌때까지 반복
-    arr = queue.popleft() # x, y, 거리
-    if arr[0]==n-1 and arr[1]==m-1:
-        print(arr[2])
-        exit()
+while queue:
+    cur = queue.popleft()
     for i in range(4):
-        nx = arr[0] + dx[i]
-        ny = arr[1] + dy[i]
-        if nx < 0 or ny < 0 or nx >= n or ny >= m or matrix[nx][ny] == 0: continue
-        if matrix[nx][ny] == 1:
-            matrix[nx][ny] = arr[2] + 1
-            queue.append([nx, ny, arr[2] + 1])
-print(arr[2])
+        nx = dx[i] + cur[0]
+        ny = dy[i] + cur[1]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m or maze[nx][ny] != 1: continue
+        queue.append((nx, ny, cur[2]+1))
+        maze[nx][ny] = -1
+
+        if nx == n-1 and ny == m-1:
+            print(cur[2]+1)
+            exit()
