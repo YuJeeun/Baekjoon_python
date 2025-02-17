491c7386dd0cf7a7a83865a586b8eb677fe07254
YuJeeun <dbwpdms1122@naver.com>
2025-02-17 17:01:43 +0900

    [Silver II] Title: 유기농 배추, Time: 68 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,37 @@
 import sys
 from collections import deque
+
 inp = sys.stdin.readline
 
 t = int(inp())
-cnt = 0
-queue = deque()
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-cur = [0]*2
-res = []
-while cnt<t:
-    bug = 0
+for _ in range(t):
     m, n, k = map(int, inp().split())
-    matrix = [[0]*m for _ in range(n)]
-    c = [list(map(int, inp().split())) for _ in range(k)]
-    cnt+=1
+    land = [[0]*n for _ in range(m)]
+    xy = [list(map(int, inp().split())) for _ in range(k)]
+    
     for i in range(k):
-        matrix[c[i][1]][c[i][0]] = 1
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] == 1:
-                queue.append([i, j])
-                matrix[i][j] = 2
-                while len(queue) != 0:
+        land[xy[i][0]][xy[i][1]] = 1
+
+    queue = deque()
+    cur = [0]*2
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    cnt = 0
+    
+    for i in range(m):
+        for j in range(n):
+            if land[i][j] == 1:
+                land[i][j] == -1
+                queue.append((i, j))
+                while queue:
                     cur = queue.popleft()
                     for k in range(4):
-                        nx = cur[0] + dx[k]
-                        ny = cur[1] + dy[k]
-                        if nx < 0 or ny < 0 or nx >= n or ny >= m or matrix[nx][ny] == 0: continue
-                        if matrix[nx][ny] == 1:
-                            matrix[nx][ny] = 2
-                            queue.append([nx, ny])
-                bug += 1
-                
-    res.append(bug)
-
-for i in range(t):
-    print(res[i])
+                        nx = dx[k] + cur[0]
+                        ny = dy[k] + cur[1]
+                        if nx < 0 or ny < 0 or nx >= m or ny >= n or land[nx][ny] != 1: continue
+                        land[nx][ny] = -1
+                        queue.append((nx, ny))
+                cnt += 1
+    print(cnt)
