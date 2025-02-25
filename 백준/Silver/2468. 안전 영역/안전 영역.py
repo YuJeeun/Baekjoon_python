f199ad50d5984d92f83df03adbd58f1305e1fc1f
YuJeeun <dbwpdms1122@naver.com>
2025-02-25 13:22:26 +0900

    [Silver I] Title: 안전 영역, Time: 1532 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,40 +1,39 @@
 import sys
 from collections import deque
-
 inp = sys.stdin.readline
 
 n = int(inp())
 
 area = [list(map(int, inp().split())) for _ in range(n)]
 
-queue = deque()
-cur = [0]*2
-visit = [[0]*n for _ in range(n)]
+maxx = max(area[0])
+minn = min(area[0])
+for i in range(1, len(area)):
+    maxx = max(maxx, max(area[i]))
+    minn = min(minn, min(area[i]))
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-h = 0
-max_h = 100
 
-res = []
-while h <= max_h:
-    safe_area = 0
-    visit = [[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if area[i][j] > h and visit[i][j]==0:
-                queue.append([i, j])
-                visit[i][j] = 1
+queue = deque()
+res= []
+for i in range(minn-1, maxx+1):
+    cnt = 0
+    visit = [[False]*n for _ in range(n)]   
+    for j in range(n):
+        for k in range(n):
+            if area[j][k] > i and not visit[j][k]:
+                visit[j][k] = True
+                queue.append((j, k))
                 while queue:
-                    cur = queue.popleft()
-                    for k in range(4):
-                        nx = dx[k] + cur[0]
-                        ny = dy[k] + cur[1]
-                        if nx < 0 or ny < 0 or nx >= n or ny >= n or visit[nx][ny] == 1 or area[nx][ny] <= h: continue
-                        visit[nx][ny] = 1
-                        queue.append([nx, ny])
-                safe_area += 1
-    res.append(safe_area)
-    h += 1
+                    cur_x, cur_y = queue.popleft()
+                    for l in range(4):
+                        nx = cur_x + dx[l]
+                        ny = cur_y + dy[l]
+                        if nx < 0 or ny < 0 or nx >= n or ny >= n or visit[nx][ny] or area[nx][ny] <= i: continue
+                        visit[nx][ny] = True
+                        queue.append((nx, ny))
+                cnt += 1
+    res.append(cnt)
+
 print(max(res))
-
-
