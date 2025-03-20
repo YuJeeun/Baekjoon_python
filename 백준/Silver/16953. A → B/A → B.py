a191c79511c2e7354fc3329960f4702c90e33be3
YuJeeun <dbwpdms1122@naver.com>
2025-03-20 17:35:54 +0900

    [Silver II] Title: A → B, Time: 128 ms, Memory: 35028 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+inp = sys.stdin.readline
+
+a, b = map(int, inp().split())
+
+queue = deque()
+
+queue.append((a, 1))
+
+while queue:
+    cur, cnt = queue.popleft()
+    if cur == b:
+        print(cnt)
+        exit()
+    dx = [cur*2, int(str(cur)+'1')]
+    for i in range(2):
+        nx = dx[i]
+        if nx > b: continue
+        queue.append((nx, cnt+1))
+
+print(-1)
