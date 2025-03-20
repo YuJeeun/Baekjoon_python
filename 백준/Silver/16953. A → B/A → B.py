0d11598e1f7bd91179c57106b7fe3ac02c05cac7
YuJeeun <dbwpdms1122@naver.com>
2025-03-20 17:38:25 +0900

    [Silver II] Title: A → B, Time: 84 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -8,15 +8,16 @@ queue = deque()
 
 queue.append((a, 1))
 
-while queue:
-    cur, cnt = queue.popleft()
-    if cur == b:
-        print(cnt)
-        exit()
-    dx = [cur*2, cur*10+1]
-    for i in range(2):
-        nx = dx[i]
-        if nx > b: continue
-        queue.append((nx, cnt+1))
-
-print(-1)
+def bfs():
+    while queue:
+        cur, cnt = queue.popleft()
+        if cur == b:
+            print(cnt)
+            exit()
+        dx = [cur*2, cur*10+1]
+        for i in range(2):
+            nx = dx[i]
+            if nx > b: continue
+            queue.append((nx, cnt+1))
+    print(-1)
+bfs()
