e5c94e472befedd7e2bf08282465324aca58f5b8
YuJeeun <dbwpdms1122@naver.com>
2025-03-28 14:38:27 +0900

    [level 2] Title: 튜플, Time: 512.81 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -3,25 +3,37 @@ def solution(s):
     answer = result(s)
     return answer
 
+# def result(str):
+#     ddict = defaultdict(int)
+#     llist = ''
+#     for s in str:
+#         if s == '{' or s == '}' or s == ',':
+#             if not llist:
+#                 continue
+#             else:
+#                 ddict[int(llist)] += 1
+#                 llist = ''
+#         else:
+#             llist += s
+    
+#     result = [0]*len(ddict)
+#     for key, value in ddict.items():
+#         result[len(ddict)-value] = key
 
-
-
+#     return result
 
 def result(str):
-    ddict = defaultdict(int)
-    llist = ''
-    for s in str:
-        if s == '{' or s == '}' or s == ',':
-            if not llist:
-                continue
-            else:
-                ddict[int(llist)] += 1
-                llist = ''
-        else:
-            llist += s
-    
-    result = [0]*len(ddict)
-    for key, value in ddict.items():
-        result[len(ddict)-value] = key
+    result = []
+    str = str.lstrip('{').rstrip('}').split('},{')
+    new_s = []
+    for i in str:
+        new_s.append(i.split(','))
+    new_s.sort(key = len)
 
+    for i in range(len(new_s)):
+        for s in new_s[i]:
+            if int(s) not in result:
+                result.append(int(s))
     return result
+    
+                      
