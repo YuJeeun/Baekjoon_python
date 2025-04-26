7e73a875fae25faa8d27a2e1912d55c8e5bfbc9f
YuJeeun <dbwpdms1122@naver.com>
2025-04-26 12:54:00 +0900

    [level 2] Title: 모음 사전, Time: 1.21 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,30 @@
+from itertools import product
 def solution(word):
-    answer = 0
-    vowels = "AEIOU"
+    vowels = ['A', 'E', 'I', 'O', 'U']
     wlist = []
-    def words(l, wdict):
-        if l == 5:
-            return
-        for i in range(len(vowels)):
-            wlist.append(wdict+vowels[i])
-            words(l+1, wdict+vowels[i])
+    all_word = []
+    for i in range(1, 6):
+        wlist.append(list(map("".join, product(vowels, repeat = i))))
+    for x in wlist:
+        for y in x:
+            all_word.append(y)
+    all_word.sort()
+    
+    return all_word.index(word)+1
+
+
+
+# def solution(word):
+#     answer = 0
+#     vowels = "AEIOU"
+#     wlist = []
+#     def words(l, wdict):
+#         if l == 5:
+#             return
+#         for i in range(len(vowels)):
+#             wlist.append(wdict+vowels[i])
+#             words(l+1, wdict+vowels[i])
         
-    words(0, '')
+#     words(0, '')
         
-    return wlist.index(word)+1
+#     return wlist.index(word)+1
